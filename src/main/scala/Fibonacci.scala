af50afd78107109189f41c958faed0ef9232e11b
PrafulBangar <prafulbangar581@gmail.com>
2019-07-11 10:19:01 +0530

    with changes

--- src/main/scala/Fibonacci.scala
+++ src/main/scala/Fibonacci.scala
@@ -1,6 +1,5 @@
 object Fibonacci extends  App{
-
-  Fibonacci.printFib(7)
+Fibonacci.printFib(7)
 
 def printFib(num:Int) ={
   for (i <- 0 until num) {
@@ -8,14 +7,17 @@ def printFib(num:Int) ={
     print(fibbonacci(i))
   }
 }
+
+
   def fibbonacci(n: Int): Int = {
-    def fib_tail(n: Int, a: Int=0, b: Int=1): Int = {
+    def tailRecursion(n: Int, a: Int=0, b: Int=1): Int = {
       n match {
         case 0 => a
-        case _ => fib_tail(n - 1, b, a + b)
+       case 1=>b
+        case _ => tailRecursion(n - 1, b, a + b)
       }
     }
-    fib_tail(n, 0 , 1)
+    tailRecursion(n, 0 , 1)
   }
 }
 //0 1 1 2 3 5
